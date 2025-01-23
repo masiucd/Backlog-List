837563c4aa514c4e427f6ceb763c5abd4b18fb87
marcell ciszek <ciszekmarcell@gmail.com>
2025-01-23 15:15:49 +0100

    Remove unused files and configurations, and add utility functions for calculator app

---
+++ apps/calculator/src/main.ts
@@ -0,0 +1,30 @@
+
+import { setupCounter } from './counter.ts'
+import{addFn} from '@recursive-ui/utils'
+
+
+let n = 1
+
+
+function render(){
+  document.querySelector<HTMLDivElement>('#app strong')!.textContent = n.toString()
+}
+
+function onClick(){
+  n = addFn(n, 1)
+  render()
+}
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1>
+<strong>${n}</strong>
+  Hello World! <button  id='btn' >Counter</button>
+</h1>
+`
+
+let btn = document.querySelector<HTMLButtonElement>('#btn')!
+btn.addEventListener('click', onClick)
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
